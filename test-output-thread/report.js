a32ff4aa303c10cc740920dc58f8e248c5d42a32
Kamal545 <kmlsarath0991@gmail.com>
2021-07-30 12:46:58 +0530

    property url code updated in Accountspagesteps

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,102 +1,36 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "534bc8fc-dab5-4c4f-a2f5-234517ec0cc9",
-    "feature": "Login page feature",
-    "scenario": "Login with incorrect credentials",
-    "start": 1624680936504,
-    "group": 22,
-    "content": "",
-    "tags": "@smoke,",
-    "end": 1624681139801,
-    "className": "passed"
-  },
-  {
-    "id": "df0d0a17-151a-4506-b2cc-3fd34c6e1ae5",
-    "feature": "Login page feature",
-    "scenario": "Forgot Password link",
-    "start": 1624680936357,
-    "group": 18,
-    "content": "",
-    "tags": "",
-    "end": 1624681085593,
-    "className": "passed"
-  },
-  {
-    "id": "20cdd012-8f44-44f0-a078-df1b50478986",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts section count",
-    "start": 1624680936536,
-    "group": 15,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1624681161255,
-    "className": "passed"
-  },
-  {
-    "id": "1da7ceed-6069-4e7f-b076-331b9275e3cf",
-    "feature": "Login page feature",
-    "scenario": "Login with incorrect credentials",
-    "start": 1624680936511,
-    "group": 21,
-    "content": "",
-    "tags": "@smoke,",
-    "end": 1624681134312,
-    "className": "passed"
-  },
-  {
-    "id": "a9172caf-7087-4e86-9034-3a75af9436c0",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts page title",
-    "start": 1624680936510,
-    "group": 14,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1624681142363,
-    "className": "passed"
-  },
-  {
-    "id": "f1849b0b-8ee5-4825-af35-f8d3d282c01b",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1624680936544,
-    "group": 17,
-    "content": "",
-    "tags": "",
-    "end": 1624681083711,
-    "className": "passed"
-  },
-  {
-    "id": "d8855256-197f-49c2-9010-e97ee4e76ae1",
+    "id": "f7cb7e1c-95db-4502-82dd-9bae847907f5",
     "feature": "Account Page Feature",
     "scenario": "Accounts page Store Information",
-    "start": 1624680936547,
+    "start": 1627626994367,
     "group": 16,
     "content": "",
     "tags": "@accounts,@smoke,",
-    "end": 1624681144638,
+    "end": 1627627221387,
     "className": "passed"
   },
   {
-    "id": "d29cd8bb-b43d-4b9f-a9df-a8f3873b7e33",
-    "feature": "Login page feature",
-    "scenario": "Login with correct credentials",
-    "start": 1624680936506,
-    "group": 19,
+    "id": "9105d698-df97-4c64-aac8-a6fcf6923ffb",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1627626994365,
+    "group": 14,
     "content": "",
-    "tags": "",
-    "end": 1624681135040,
+    "tags": "@accounts,",
+    "end": 1627627223009,
     "className": "passed"
   },
   {
-    "id": "54451ad0-9428-4cce-a212-967fb8a4cd84",
-    "feature": "Login page feature",
-    "scenario": "Login with incorrect credentials",
-    "start": 1624680936503,
-    "group": 20,
+    "id": "dce9af8e-f52b-4426-ba0d-2af044b9ff6e",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1627626994363,
+    "group": 15,
     "content": "",
-    "tags": "@smoke,",
-    "end": 1624681142314,
+    "tags": "@accounts,",
+    "end": 1627627218738,
     "className": "passed"
   }
 ]);
@@ -112,30 +46,6 @@ CucumberHTML.timelineGroups.pushArray([
   {
     "id": 16,
     "content": "Thread[TestNG-PoolService-2,5,main]"
-  },
-  {
-    "id": 17,
-    "content": "Thread[TestNG-PoolService-3,5,main]"
-  },
-  {
-    "id": 18,
-    "content": "Thread[TestNG-PoolService-4,5,main]"
-  },
-  {
-    "id": 19,
-    "content": "Thread[TestNG-PoolService-5,5,main]"
-  },
-  {
-    "id": 20,
-    "content": "Thread[TestNG-PoolService-6,5,main]"
-  },
-  {
-    "id": 21,
-    "content": "Thread[TestNG-PoolService-7,5,main]"
-  },
-  {
-    "id": 22,
-    "content": "Thread[TestNG-PoolService-8,5,main]"
   }
 ]);
 });
